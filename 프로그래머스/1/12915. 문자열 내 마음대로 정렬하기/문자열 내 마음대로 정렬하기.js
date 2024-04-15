0b963de88f628a45c78f05a53ba69ae23475f2c1
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-04-15 09:29:14 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -2,6 +2,6 @@ function solution(strings, n) {
     return strings.sort((a, b) => {
         if (a[n] !== b[n]) {
             return a[n] > b[n] ? 1 : -1;
-        } return a > b ? 1 : -1;
+        } return a > b ? 1: -1;
     })
 }
